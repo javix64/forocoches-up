5f17251d3c7ce3b0a0a23ef130a8e75a18a27252
javix64 <jav1.x64@gmail.com>
2021-05-15 21:09:55 +0200

    Terminado

--- README.md
+++ README.md
@@ -3,11 +3,35 @@ Esto es un upeador de posts de [forocoches](https://forocoches.com)
 
 ## EN CONSTRUCCION
 pues eso lo voy a dejar de lado una temporada
+
 ## Requirements
     - Selenium
     - Raspberry?
+
+## Disclaimer:
+
+
+No soy tu soporte tecnico, si no sabes donde te metes, no preguntes.
+
+
 ## How start
 
+1- Teneis que abrir el archivo y editar la url de la carpeta de firefox para que se conecte automaticamente sin hacer login, coge las cookies y ya estas dentro.
+
+2- Por defecto, el tiempo entre respuesta y respuesta es de 60segundos, lo puedes cambiar en `time.sleep(XX)`, obvio: min: 30
+
+3- Cambiar cuantos mensajes quieres escribir, en el bucle ` for i in range(1,200)`, esta puesto 200 mensajes, puedes cambiarlo a lo que quieras.
+
+
+Parametros: 
+
+`{URL}` es la url del sitio, ejemplo: `https://www.forocoches.com/foro/showthread.php?p=402553270`
+
+
+`python3 main.py {URL}`
+
+osea que se quedaria asi:
+`python3 main.py https://www.forocoches.com/foro/showthread.php?p=402553270`
 
 ## Why?
 Porque puedo y porque no hay, si te gusta dale amor con estrellitas y conviame a un cafelillo.
--- main.py
+++ main.py
@@ -1,16 +1,13 @@
 import time, sys
 from selenium import webdriver
-from selenium.webdriver.chrome.options import Options
-username = sys.argv[1]
-password = sys.argv[2]
-post = sys.argv[3]
-
-driver = webdriver.Firefox()
-driver.get("https://www.forocoches.com/foro/misc.php?do=page&template=ident")
-driver.find_element_by_xpath('/html/body/div[5]/div[2]/div/div/div/div/div/div/div[2]/div[1]/button[1]').click()
-time.sleep(2)
-driver.find_element_by_name('vb_login_username').send_keys(username)
-driver.find_element_by_name('vb_login_password').send_keys(password)
-time.sleep(1)
-driver.find_element_by_name('logb2').click()
-document.find_element_by_id('checkbox').click()
+post = sys.argv[1]
+carpetaFirefox = '/home/x535/.mozilla/firefox/23nz704b.default-release/'
+profile = webdriver.FirefoxProfile(carpetaFirefox)
+driver = webdriver.Firefox(firefox_profile=profile)
+driver.get(post)
+for i in range(2,200):
+    textarea = driver.find_element_by_id('vB_Editor_QR_iframe')
+    textarea.send_keys('+',i)
+    buttonRespond = driver.find_element_by_id('qr_submit')
+    buttonRespond.click()
+    time.sleep(60)
